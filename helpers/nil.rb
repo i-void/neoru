4392116647874a99814efea870bfba7cce776a0a
Onur Eren Elibol <onurelibol@gmail.com>
2014-01-22 17:07:00 +0200

    [completed: 5]

--- helpers/nil.rb
+++ helpers/nil.rb
@@ -1,5 +1,9 @@
 class NilClass
-  def blank?
-    true if defined?(self).nil? or self.nil?
-  end
+	def blank?
+		true if defined?(self).nil? or self.nil?
+	end
+
+	def length
+		return 0
+	end
 end
