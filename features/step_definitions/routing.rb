3d844f29b2b86ab4f9573998fae870d63fc49cfd
Onur Eren Elibol <onurelibol@gmail.com>
2014-09-22 18:30:44 +0300

    [completed: 6]

---
+++ features/step_definitions/routing.rb
@@ -0,0 +1,15 @@
+Given(/^add route as \/test with parameters will tested by (.*)$/) do |regexp|
+  Neo::Config.main[:routes][:test] = ['/test', "#{regexp}", 'main:main:index', 'get']
+  Neo.server_vars['REQUEST_METHOD'] = 'get'
+end
+
+Then(/^(.*) must pass$/) do |true_url|
+  Neo.server_vars['REQUEST_PATH'] = true_url
+  expect(Neo::Router.check_from_config).to eq('main:main:index')
+end
+
+
+Then(/^(.*) must give error$/) do |false_url|
+  Neo.server_vars['REQUEST_PATH'] = false_url
+  expect(Neo::Router.check_from_config).to eq(nil)
+end
