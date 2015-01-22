53893446ce219f5fec67f76ee70d58d9569871de
Onur Eren Elibol <onurelibol@gmail.com>
2015-01-22 23:25:44 +0200

    model generator command

--- commands/model_generator/a_model.rb
+++ commands/model_generator/a_model.rb
@@ -3,8 +3,8 @@ class Neo::Commands::ModelGenerator::AModel
 		@module = module_name
 		@name = name.to_s
 		@properties = data[:HasProperties]
-		@has_many = data[:HasMany]
-		@has_one = data[:HasOne]
+		@has_many = data[:HasMany] || {}
+		@has_one = data[:HasOne] || {}
 	end
 
 	def make_file(module_path)
@@ -19,10 +19,36 @@ class Neo::Commands::ModelGenerator::AModel
 		end
 	end
 
+	def write_has_many_relations
+		unless @has_many.blank?
+			@has_many.each do |relation, model|
+				model_name = (model.kind_of? Hash)? model.values[0] : model
+				@file.puts ''
+				@file.puts "  def get_all_#{relation.to_s.underscore}"
+				@file.puts "    #{model_name}Query.new.belongs_to_data(self).find"
+				@file.puts '  end'
+			end
+		end
+	end
+
+	def write_constants
+		models = [@name] + @has_many.values + @has_one.values
+		models.uniq.each do |model|
+			_module, _name =
+				if model.kind_of?(Hash)
+					[model.keys[0], model.values[0]]
+				else
+					[@module, model]
+				end
+			@file.puts "  #{_name}Query = #{_module}::Models::#{_name}Query"
+		end
+	end
+
 	def write_class
 		@file.puts "class #{@module}::Models::#{@name} < Neo::Database::Model"
-		@file.puts "  #{@name}Query = #{@module}::Models::#{@name}Query"
+		write_constants
 		write_accessors
+		write_has_many_relations
 		@file.puts 'end'
 	end
 
