6b302995a56620892622537008b6dc41be6be9c7
Onur Eren Elibol <onurelibol@gmail.com>
2015-02-15 19:44:53 +0200

    nova user auth, neoru path method in controller

--- controller.rb
+++ controller.rb
@@ -9,17 +9,21 @@ module Neo
       else
         params[:content] =  Neo::View.new.render(params)
         content = Neo::View.new(@layout).render(params)
-        Neo::Response.html(content)
+        Neo::Response.html content
       end
     end
 
     def redirect(url)
-      Neo::Response.redirect(url)
+      Neo::Response.redirect url
+    end
+
+    def path(name, parameters=[])
+	    Neo.generate_url name, parameters
     end
 
     def render_view(view,params)
       content = Neo::View.new(view).render(params)
-      Neo::Response.html(content)
+      Neo::Response.html content
     end
   end
 end
